7128d5b6676a1bae6e80f59808dc091f16dab17a
shuo li <shuoli84@gmail.com>
2015-02-24 23:27:47 +0800

    Fix #1, set the PATH for sudoer too. Bump version

--- lib/actions/configure_peach.rb
+++ lib/actions/configure_peach.rb
@@ -4,29 +4,31 @@ module VagrantPeach
       include Vagrant::Util
 
       def initialize(app, env)
-        @app    = app
+        @app = app
       end
 
       def call(env)
-        env[:ui].info "in the peach configure action"
-        static_files = File.expand_path('../../tools', __FILE__)
+        env[:ui].info 'Configure peach'
 
-        env[:ui].info static_files
+        static_files = File.expand_path('../../tools', __FILE__)
+        env[:ui].info "Copy files from #{static_files} to /peach/tools"
 
         machine = env[:machine]
-        machine.communicate.sudo "mkdir -p /peach/tools"
-        machine.communicate.sudo "chown vagrant:vagrant /peach/tools"
+        machine.communicate.sudo 'mkdir -p /peach/tools'
+        machine.communicate.sudo 'chown vagrant:vagrant /peach/tools'
 
         # copy tools into /peach/tools
-        machine.communicate.upload "#{static_files}/command.py", "/peach/tools/command.py"
-        machine.communicate.upload "#{static_files}/wget", "/peach/tools/wget"
-        machine.communicate.upload "#{static_files}/curl", "/peach/tools/curl"
+        machine.communicate.upload "#{static_files}/command.py", '/peach/tools/command.py'
+        machine.communicate.upload "#{static_files}/wget", '/peach/tools/wget'
+        machine.communicate.upload "#{static_files}/curl", '/peach/tools/curl'
 
-        # Set the path
-        machine.communicate.sudo 'echo "export PATH=/peach/tools:$PATH" > /etc/profile.d/peach.sh'
+        env[:ui].info 'Configure peach server to 10.0.2.2:5000'
+        machine.communicate.sudo 'mkdir -p /etc/peach'
+        machine.communicate.sudo 'echo {\\"server\\":\\"10.0.2.2:5000\\"} > /etc/peach/conf.json'
 
-        # Setup the peach server
-        machine.communicate.execute 'echo {\\"server\\":\\"10.0.2.2:5000\\"} > ~/.peach.json'
+        env[:ui].info 'Setup the PATH env'
+        machine.communicate.sudo %Q(sed -e 's|/peach/tools:||g' -i /etc/environment && sed -e 's|PATH="\\(.*\\)"|PATH="/peach/tools:\\1"|g' -i /etc/environment)
+        machine.communicate.sudo %Q(sed -e 's|/peach/tools:||g' -i /etc/sudoers && sed -e 's|secure_path="\\(.*\\)"|secure_path="/peach/tools:\\1"|g' -i /etc/sudoers)
 
         @app.call env
       end
--- lib/vagrant-peach/version.rb
+++ lib/vagrant-peach/version.rb
@@ -1,5 +1,5 @@
 module Vagrant
   module Peach
-    VERSION = "0.0.1"
+    VERSION = "0.0.3"
   end
 end
